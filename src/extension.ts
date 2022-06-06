ea9932e9373c7f6823585447770e7c2826517284
NoobTriesToCode <101266592+BorjaCaneda@users.noreply.github.com>
2022-06-06 15:13:17 +0200

    Revert "Copia a portapapeles"
    This reverts commit b391e3a01e299d05f4e703c25fdef12f845e114b.

--- src/extension.ts
+++ src/extension.ts
@@ -3,83 +3,47 @@
 import * as vscode from 'vscode';
 import configuracion from './configuracion.json';
 
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');            
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloworld" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async(uri:vscode.Uri) => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', (arg1: any) => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
         console.log("Ejecutando");
-        
-        /* Abrir un documento
+
         var pos1 = new vscode.Position(10, 4);
         var openPath = vscode.Uri.file('C:/Users/Ordenador/AppData/Local/Programs/Microsoft VS Code/resources/app/extensions/helloworld/src/prueba.txt');
         vscode.workspace.openTextDocument(openPath).then(doc => {
             vscode.window.showTextDocument(doc).then(editor => {
                 // Line added - by having a selection at the same position twice, the cursor jumps there
-                editor.selections = [new vscode.Selection(pos1, pos1)];
+                editor.selections = [new vscode.Selection(pos1,pos1)]; 
 
                 // And the visible range jumps there too
                 var range = new vscode.Range(pos1, pos1);
-
+                let remoteConfig;
+                            
                 editor.revealRange(range);
             });
-        });
-        */
-
-        /* Direccion del archivo que este abierto
-        var vscode2 = require("vscode");
-        var path = require("path");    
-        var currentlyOpenTabfilePath = vscode2.window.activeTextEditor.document.fileName;
-        var currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
-        
-        vscode.window.showInformationMessage(currentlyOpenTabfilePath);            
-        //vscode.window.showInformationMessage(currentlyOpenTabfileName);
+        });        
 
         const terminal = vscode.window.createTerminal();
         terminal.show();
-        terminal.sendText("copy " + currentlyOpenTabfileName + " " + configuracion.archivo);    
-        vscode.window.showInformationMessage('Hello World from ASD!');
-        */
+        terminal.sendText(configuracion.comando1);     
+        terminal.sendText(configuracion.comando2);
+        
+		vscode.window.showInformationMessage('Hello World from ASD!');
+	});
 
-        /* Direccion del explorer
-        var vscode2 = require("vscode");
-        let wf = vscode2.workspace.workspaceFolders[0].uri.path ;
-        let f = vscode2.workspace.workspaceFolders[0].uri.fsPath ; 
-
-        var message = `YOUR-EXTENSION: folder: ${wf} - ${f}` ;
-
-        vscode.window.showInformationMessage(message);
-        vscode.window.showInformationMessage(uri.fsPath);
-        */
-
-        vscode.commands.executeCommand('copyFilePath');
-
-        var originalClipboard = vscode.env.clipboard.readText();
-        var auxiliar = Promise.resolve(originalClipboard);
-        var valorPath;
-        Promise.all([auxiliar]).then(values => {
-            console.log(values);
-
-            vscode.window.showInformationMessage(values[0]);
-        });
-
-
-    });
-    
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
-  
+
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
