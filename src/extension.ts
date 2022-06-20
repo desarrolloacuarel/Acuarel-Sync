89974940079abfe5555c5ece0fe658794c8bdf39
NoobTriesToCode <101266592+BorjaCaneda@users.noreply.github.com>
2022-06-20 14:22:26 +0200

    Funcionando

--- src/extension.ts
+++ src/extension.ts
@@ -37,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
          */
 
 
-        let sincronizar = vscode.commands.registerCommand('acuarelsync.sync', fileURLToPath => {
+        let sincronizar = vscode.commands.registerCommand('acuarelsync.sync1', fileURLToPath => {
             // The code you place here will be executed every time your command is executed
             // Display a message box to the user
 
